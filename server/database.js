b20311e6053abfb69a4141265d89c8f9fc8fb145
StanTheMan132 <hettingas@ismanila.org>
2018-08-01 14:46:28 +0200

    added startDatabase

--- server/database.js
+++ server/database.js
@@ -3,8 +3,12 @@ const config = require('./config/config');
 
 const connectionString = `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`;
 
-try {
-  mongoose.connect(connectionString); 
-} catch (error) {
-  console.log(error);
+function startDatabase() {
+  try {
+    mongoose.connect(connectionString);
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+module.exports = startDatabase;
