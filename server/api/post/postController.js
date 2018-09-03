31b0ae99590e99e62d508cb2595e2cc436385e8f
StanTheMan132 <hettingas@ismanila.org>
2018-09-03 19:21:22 +0200

    moved postController logic to postFunctions

--- server/api/post/postController.js
+++ server/api/post/postController.js
@@ -8,16 +8,12 @@ exports.createNewPost = async function createNewPost(req, res, next) {
     if (!req.body.imgUrl) {
       res.status(400).json({ msg: 'No img found, please add a img' });
     }
-    const newDate = new Date();
-    console.log(req.user.id);
-    const user = await User.findOne({ _id: req.user.id });
-    const newPost = new Post({
-      username: user.username,
-      imgUrl: req.body.imgUrl,
-      date: newDate,
-    });
-    newPost.save();
-    res.status(201).json(newPost);
+    const newPost = await postFunctions.newPost(req.body.imgUrl, req.user.id, Post);
+    if (newPost.succes) {
+      res.status(201).json(newPost);
+    } else {
+      res.status(500).json(newPost);
+    }
   } catch (error) {
     next(error);
   }
@@ -25,14 +21,9 @@ exports.createNewPost = async function createNewPost(req, res, next) {
 
 exports.getLatestPost = async function getLatestPost(req, res, next) {
   try {
-    let latestPost = [];
     const { lastPostDate } = req.query;
-    if (lastPostDate) {
-      latestPost = await Post.find({ date: { $lt: lastPostDate } }).sort('-date').limit(11);
-    } else {
-      latestPost = await Post.find().sort('-date').limit(11);
-    }
-    res.status(200).send(latestPost);
+    const posts = await postFunctions.latestPost(lastPostDate, Post);
+    res.status(200).send(posts);
   } catch (err) {
     next(err);
   }
@@ -50,8 +41,6 @@ exports.deletePost = async function deletePost(req, res, next) {
     } else {
       const done = await postFunctions.deletePostandComments(req.body.postId, Post, commentController);
       res.send(done);
-      
-      
     }
   } catch (err) {
     next(err);
--- server/api/post/postFunctions.js
+++ server/api/post/postFunctions.js
@@ -1,3 +1,4 @@
+const User = require('../auth/userModel');
 
 exports.deletePostandComments = async function deletePostandComments(postId, Post, commentController) {
   try {
@@ -8,6 +9,42 @@ exports.deletePostandComments = async function deletePostandComments(postId, Pos
     }
     return false;
   } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
+
+exports.latestPost = async function latestPost(lastPostDate, Post) {
+  try {
+    let posts = [];
+    if (lastPostDate) {
+      posts = await Post.find({ date: { $lt: lastPostDate } }).sort('-date').limit(11);
+    } else {
+      posts = await Post.find().sort('-date').limit(11);
+    }
+    return posts;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
+
+exports.newPost = async function newComment(imgUrl, userId, Post) {
+  try {
+    const user = await User.findOne({ _id: userId });
+    if (user) {
+      const date = new Date();
+      const newPost = new Post({
+        username: user.username,
+        imgUrl,
+        date,
+      });
+      await newPost.save();
+      return { success: true, newPost };
+    }
+    return { success: false, msg: 'User not found' };
+  } catch (err) {
+    console.log(err);
     return err;
   }
 };
